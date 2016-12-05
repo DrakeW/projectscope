7831efbff83715e4816bb96b692678d33afbf1f3
Junyu Wang <junyuw@berkeley.edu>
2016-12-04 23:42:19 -0800

    Revert "[iter4 deploy only] Feature/timeslider ajax"

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,6 @@ Rails.application.routes.draw do
   resources :users, :only => [:show, :update], :path => "u"
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, :skip => [:password]
   
-  resources :projects do
-    collection do
-      post "/metrics_on_date", :to => "projects#metrics_on_date"
-    end
-  end
+  resources :projects
   root 'projects#index'
 end
